b0fba1d964061d0f1ba79dd12ca75a1b96e79d26
DominicDev <s0l0estudi4r@gmail.com>
2026-01-13 13:51:28 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'MY CHEMICAL ROMANCE';
+export let nameSongs = [
+	"WELCOME TO THE BLACK PARADE", /*1*/
+	"HELENA", /*2*/
+	"I'M NOT OKAY (I PROMISE)", /*3*/
+	"FAMOUS LAST WORDS", /*4*/
+	"THE GHOST OF YOU", /*5*/
+	"NA NA NA", /*6*/
+	"TEENAGERS", /*7*/
+	"DEAD!", /*8*/
+	"SING", /*9*/
+	"I DON'T LOVE YOU", /*10*/
+	"MAMA", /*11*/
+	"CEMETERY DRIVE", /*12*/
+	"GIVE 'EM HELL, KID", /*13*/
+	"THANK YOU FOR THE VENOM", /*14*/
+	"SUMMERTIME", /*15*/
+	"PLANETARY (GO!)", /*16*/
+	"THE SHARPEST LIVES", /*17*/
+	"CANCER", /*18*/
+	"VAMPIRES WILL NEVER HURT YOU", /*19*/
+	"DISENCHANTED", /*20*/
+]
